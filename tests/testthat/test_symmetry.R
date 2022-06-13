83bb88c29911f0f13251d24eafb4d4f140ce57f7
Mika Braginsky <mika.br@gmail.com>
2022-06-13 11:26:49 -0400

    change symmetry test tolerance

--- tests/testthat/test_symmetry.R
+++ tests/testthat/test_symmetry.R
@@ -7,5 +7,5 @@ test_that("symmetry wrt favor_positive", {
   res2 <- phacking_rtma(lodder$yi_flipped, lodder$vi, parallelize = FALSE,
                         favor_positive = FALSE)
 
-  expect_equal(res1$stats$mode, res2$stats$mode, tolerance = 0.001)
+  expect_equal(res1$stats$mode, res2$stats$mode, tolerance = 0.01)
 })
