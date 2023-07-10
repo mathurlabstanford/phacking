3b7fad7642bb7c741cf58a6b9214f10e09d0339c
Peter Solymos <psolymos@gmail.com>
2023-07-09 19:38:01 -0600

    Local checks using docker
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- check.sh
+++ check.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-# Usage
+## Usage
+##
+## Define and pull a Docker image
 # export IMAGE="rhub/ubuntu-release"
+# export IMAGE="eddelbuettel/r2u:22.04"
 # docker pull $IMAGE
-# docker run -it --rm -v $PWD:/home/root/phacking -w /home/root $IMAGE /bin/bash phacking/check.sh
+##
+## Clean up artifact folder
+# rm -rf _tmp
+# mkdir _tmp
+##
+## Run the check.sh script
+# docker run -it --rm --platform linux/amd64 -v $PWD:/home/root/phacking -w /home/root $IMAGE /bin/bash phacking/check.sh
+##
+## Now check files in the _tmp folder
 
-R -q -e "install.packages('devtools')"
-R -q -e "remotes::install_deps('phacking')"
-R CMD build phacking
-R_CHECK_DONTTEST_EXAMPLES=false R CMD check --no-manual --no-tests phacking_*.tar.gz
+PKG=phacking
+TMP=$PKG/_tmp
+FILE=$TMP/$PKG-check-logs.log
+echo $(date) - Script started > $FILE
+
+echo $(date) - Installing package dependencies >> $FILE
+R -q -e "remotes::install_deps('$PKG', upgrade='never', dependencies=TRUE)"
+
+echo $(date) - Building package >> $FILE
+R CMD build $PKG
+
+echo $(date) - Running checks >> $FILE
+# R_CHECK_DONTTEST_EXAMPLES=false R CMD check --no-manual --no-tests $PKG_*.tar.gz
+R_CHECK_DONTTEST_EXAMPLES=false R CMD check --no-manual --as-cran $PKG_*.tar.gz
+
+echo $(date) - Moving artifacts >> $FILE
+cp $PKG_*.tar.gz $TMP
+cp -r $PKG.Rcheck $TMP
+echo $(date) - Done >> $FILE
