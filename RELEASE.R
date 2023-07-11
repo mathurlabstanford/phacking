9fa118d878ca90c7cca92dcf7a5e379e2adc572e
Peter Solymos <psolymos@gmail.com>
2023-07-11 15:58:10 -0600

    Add RELEASE file
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ RELEASE.R
@@ -0,0 +1,38 @@
+library(rhub)
+#validate_email("psolymos@gmail.com")
+platforms()
+f <- c("ubuntu-gcc-devel",
+       "ubuntu-gcc-release",
+       "windows-x86_64-devel",
+       "windows-x86_64-release",
+       "windows-x86_64-oldrel")
+check(platform=f)
+list_package_checks(".")
+
+pkgnews <- function() {
+        x <- readLines("NEWS.md")
+        x <- x[x != ""]
+        h <- which(startsWith(x, "#"))
+        i <- (h[1]+1):(h[2]-1)
+        paste0(x[i], collapse="\n")
+    }
+cat(sprintf('Dear CRAN Maintainers,
+
+I am submitting the %s version of the %s R extension package to CRAN.
+
+## Changelog
+
+We made the following changes since the last release:
+
+%s
+
+%s
+
+Yours,
+
+Peter Solymos
+maintainer',
+       read.dcf("DESCRIPTION", fields="Version")[1],
+       read.dcf("DESCRIPTION", fields="Package")[1],
+       pkgnews(),
+       paste0(readLines("cran-comments.md"), collapse="\n")))
